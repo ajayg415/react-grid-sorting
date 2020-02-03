99b366831e6418723192000787b72a18dd59fb47
ajayg415 <ajay.g415@gmail.com>
2020-02-03 21:31:39 +0530

    added custom icons

---
+++ src/svg/ArrowMix.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ArrowMix = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M16 17.01V10h-2v7.01h-3L15 21l4-3.99h-3zM9 3L5 6.99h3V14h2V6.99h3L9 3z"/></svg>
+  );
+}
+
+export default ArrowMix;
